4111592caf24efa63e7c9fd8fecad2b6ef97348b
Samuel Raducan <samuel.thembp@gmail.com>
2023-10-11 17:17:22 +0100

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import prisma from "@/prisma/client";
 import { z } from "zod";
 
 const issueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required."),
 });
 
 export async function POST(request: NextRequest) {
